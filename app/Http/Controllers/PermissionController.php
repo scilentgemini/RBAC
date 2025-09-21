222803a93bb6cadc9c91e678eed68d389eaf4cc6
scilentgemini <rbchitthi@gmail.com>
2025-09-21 20:11:01 +0545

    permissions CRUD comeplete

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -12,7 +12,7 @@ class PermissionController extends Controller
     public function index()
     {
         $permissions = Permission::orderBy('created_at', 'asc')->paginate(10);
-        return view('permissions.list',[
+        return view('permissions.list', [
             'permissions' => $permissions
         ]);
     }
@@ -26,14 +26,13 @@ class PermissionController extends Controller
     // this method will insert a permission in DB
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'name' => 'required|unique:permissions|min:3'
         ]);
 
         if ($validator->passes()) {
             Permission::create(['name' => $request->name]);
             return redirect()->route('permissions.index')->with('success', 'Permission added successfully.');
-
         } else {
             return redirect()->route('permissions.create')->withInput()->withErrors($validator);
         }
@@ -47,14 +46,13 @@ class PermissionController extends Controller
         return view('permissions.edit', [
             'permission' => $permission
         ]);
-
     }
 
     // this method will update a permission in DB
     public function update($id, Request $request)
     {
-         $validator = Validator::make($request->all(),[
-            'name' => 'required|min:3|unique:permissions,name,'.$id.',id'
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3|unique:permissions,name,' . $id . ',id'
         ]);
 
         if ($validator->passes()) {
@@ -65,15 +63,29 @@ class PermissionController extends Controller
             $permission->save();
 
             return redirect()->route('permissions.index')->with('success', 'Permission updated successfully.');
-
         } else {
             return redirect()->route('permissions.edit', $id)->withInput()->withErrors($validator);
         }
     }
 
     // this method will delete a permission in DB
-    public function destroy()
+    public function destroy(Request $request)
     {
-        
+        $id = $request->id;
+
+        $permission = Permission::find($id);
+
+        if ($permission == null) {
+            session()->flash('error', 'Permission not found.');
+            return response()->json([
+                'status' => false
+            ]);
+        }
+
+        $permission->delete();
+        session()->flash('success', 'Permission deleted successfully.');
+        return response()->json([
+            'status' => true
+        ]);
     }
 }
--- resources/views/permissions/list.blade.php
+++ resources/views/permissions/list.blade.php
@@ -37,8 +37,10 @@
                                 <td class="px-6 py-3 text-center">
                                     <a href="{{ route('permissions.edit', $permission->id) }}"
                                         class="bg-slate-700 text-sm rounded-md px-3 py-2 text-white hover:bg-slate-600">Edit</a>
-                                    <a href="#"
-                                        class="bg-red-600 text-sm rounded-md px-3 py-2 text-white hover:bg-red-500">Delete</a>
+                                    <a href="javascript:void(0);" onClick="deletePermission({{ $permission->id }})"
+                                        class="bg-red-600 text-sm rounded-md px-3 py-2 text-white hover:bg-red-500">
+                                        Delete
+                                    </a>
                                 </td>
                             </tr>
                         @endforeach
@@ -51,4 +53,24 @@
             </div>
         </div>
     </div>
+    <x-slot name="script">
+        <script type="text/javascript">
+        function deletePermission(id){
+            if(confirm("Are you sure you want to delete?")) {
+                $.ajax({
+                    url : '{{ route("permissions.destroy") }}',
+                    type: 'delete',
+                    data: {id:id},
+                    dataType: 'json',
+                    headers: {
+                        'x-csrf-token' : '{{ csrf_token() }}'
+                    },
+                    success: function(response) {
+                        window.location.href = '{{ route("permissions.index") }}';
+                    }
+                })
+            }
+        }
+        </script>
+    </x-slot>
 </x-app-layout>
